d06728ac4c9f40673446c3ed67a637a138b01d6e
milena-lib <57179624+milena-lib@users.noreply.github.com>
2020-11-07 20:01:05 +0200

    update data

--- src/app/pages/reactive-forms/reactive-forms.component.ts
+++ src/app/pages/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -6,12 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-forms.component.css']
 })
 export class ReactiveFormsComponent implements OnInit {
-  
+form: FormArray; 
+ 
 bursaIsrael = [
   {
     name: 'ת"א 35',
     dailyChange: '0.29%',
     idx: '1,373.98',
+    selected: '',
+    fieldType: 'SELECT',
     securities: [
       { name: 'טבע', value: 'teva'},
       { name: 'פועלים', value: 'pohalim'},
@@ -28,6 +32,8 @@ bursaIsrael = [
     name: 'ת"א 125',
     dailyChange: '0.38%',
     idx: '1,416.01',
+    selected: '',
+    fieldType: 'SELECT',
     securities: [
       { name: 'הראל השקעות', value: 'harel'},
       { name: 'סאפיינס', value: 'sapeince'},
@@ -44,22 +50,26 @@ bursaIsrael = [
     name: 'ת"א גלובל בלוקט',
     dailyChange: '0.37%',
     idx: '497.90',
+    selected: '',
+    fieldType: 'INPUT',
     securities: [
-      { name: 'קמטק', value: 'kamtech'},
-      { name: 'מטריקס', value: 'matrix'},
-      { name: 'פורמולה מערכות', value: 'formulaSystem'},
-      { name: 'דורל אנרגיה', value: 'durelEnergy'},
-      { name: 'ארד', value: 'arad'},
-      { name: 'אלוט', value: 'alot'},
-      { name: 'סומוטו', value: 'somoto'},
-      { name: 'מיט-טק', value: 'mitTech'},
-      { name: 'איתמר', value: 'itamar'}
+      // { name: 'קמטק', value: 'kamtech'},
+      // { name: 'מטריקס', value: 'matrix'},
+      // { name: 'פורמולה מערכות', value: 'formulaSystem'},
+      // { name: 'דורל אנרגיה', value: 'durelEnergy'},
+      // { name: 'ארד', value: 'arad'},
+      // { name: 'אלוט', value: 'alot'},
+      // { name: 'סומוטו', value: 'somoto'},
+      // { name: 'מיט-טק', value: 'mitTech'},
+      // { name: 'איתמר', value: 'itamar'}
     ]
   },
   {
     name: 'ת"א 90',
     dailyChange: '0.56%',
     idx: '1,505.14',
+    selected: '',
+    fieldType: 'SELECT',
     securities: [
       { name: 'מגדל ביטוח', value: 'migdal'},
       { name: 'ישראכרט', value: 'isracard'},
@@ -74,9 +84,38 @@ bursaIsrael = [
   }
 ];
    
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    // this.form = this.createForm(null);
+  }
+
+  // createForm(data:any[]):FormArray
+  // {
+  //    let controls:FormGroup[]=
+  //       data?data.map(x=>this.createLineArray(x))
+  //           :[this.createLineArray(null)];
+  //    return this.fb.array(controls);
+  // }
+
+  // createLineArray(data:any):FormGroup
+  // {
+  //     return this.fb.group({
+  //       field1:[data && data.field2?data.field2:null,Validators.required],
+  //       field2:[data && data.field1?data.field1:null,Validators.required]
+  //     })
+  // }
+
+  onChangeSelected($event,i) {
+    this.bursaIsrael[i].selected = $event.value;
+  }
+
+  onChangeInput($event, i) {
+    this.bursaIsrael[i].selected = $event.target.value;
+  }
+
+  send() {
+    console.log("bursaIsrael: ", this.bursaIsrael);
   }
 
 }
